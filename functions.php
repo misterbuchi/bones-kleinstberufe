297f858a2bf307612d92fd302538891bcb9d35fb
Fussverkehr <dominik.bucheli@fussverkehr.ch>
2019-07-25 20:38:23 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -259,4 +259,12 @@ function kb_svg ( $svg_mime ){
 
 add_filter( 'upload_mimes', 'kb_svg' );
 
+add_theme_support( 'custom-logo', array(
+	'height'      => 127,
+	'width'       => 300,
+	'flex-height' => true,
+	'flex-width'  => true,
+	'header-text' => array( 'site-title', 'site-description' ),
+) );
+
 /* DON'T DELETE THIS CLOSING TAG */ ?>
