55a08eddbcceb4462b0b486f6f09225933234c3e
Dominik Bucheli <dominik@bucheli.me>
2019-07-27 14:09:04 +0200

    Update header.php

--- header.php
+++ header.php
@@ -21,6 +21,8 @@
 		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
 		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
 		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/lightbox.min.css" />
+		
 		
 		<!--[if IE]>
 			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
