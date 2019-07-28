86f9521020a991700809e73bf4198d84a2e4bf4b
Dominik Bucheli <dominik@bucheli.me>
2019-07-28 17:59:52 +0200

    Update single.php

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-			<div id="content">
+			<div id="content" class="page">
 										<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				
 				<div class="Rectangle pale-lilac first"></div>
