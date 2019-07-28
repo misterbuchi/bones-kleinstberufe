fac142628019ec7619863e2965bb108c1fc7860c
Dominik Bucheli <dominik@bucheli.me>
2019-07-28 18:10:17 +0200

    Update single.php

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
 				<ul class="register">
 					<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Startseite</a></li>
 				
-					<li><?php  get_the_category_list('</li><li>'); ?></li>
+					<li><?php  single_cat_title(); ?></li>
 					
 					
 					<li><?php the_title(); ?></li>
