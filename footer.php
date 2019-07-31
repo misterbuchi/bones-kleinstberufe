ffefba8228879c0680434a1c09cd725e1b56afbe
Dominik Bucheli <dominik@bucheli.me>
2019-07-31 14:35:38 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -38,11 +38,13 @@
 					
 					<div id="sponsoren">
 						<?php
-						                    query_posts( array('cat' => 16, 'posts_per_page' => -1, 'order' => 'ASC') );
-						                    while(have_posts()) : the_post();
+						                    query_posts( array('cat' => 16, 'posts_per_page' => -1, 'order' => 'ASC') );?>
+						                    											<h4><?php single_cat_title(); ?>.</h4>
+						                    
+						                    
+						                    <?php while(have_posts()) : the_post();
 						                    ?>
 											
-											<h4><?php single_cat_title(); ?>.</h4>
 						
 						                    <div class="sponsoren">
 						
