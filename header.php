caebe3b22ce2b2354013f183bf05c271bacc0285
Dominik Bucheli <dominik@bucheli.me>
2019-08-10 16:29:19 +0200

    hamburg

--- header.php
+++ header.php
@@ -68,29 +68,28 @@
 
 
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-									<div class="m-menu t-h0 d-h0">									    
+									<div class="m-menu t-h0 d-h0">									    <input type="checkbox" id="hamburg" class="nav-trigger t-h0 d-h0 m-all" role="tooltip">
 									    <label for="hamburg" class="hamburg">
 									        <span class="line"></span>
 									        <span class="line"></span>
 									        <span class="line"></span>
 									    </label>
+													<?php wp_nav_menu(array(
+										         'container' => false,                           // remove nav container
+										         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+										         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+										         'menu_class' => 'nav top-nav cf',               // adding custom nav class
+										         'theme_location' => 'main-nav',                 // where it's located in the theme
+										         'before' => '',                                 // before the menu
+										           'after' => '',                                  // after the menu
+										           'link_before' => '',                            // before each link
+										           'link_after' => '',                             // after each link
+										           'depth' => 0,                                   // limit the depth of the nav
+										         'fallback_cb' => ''                             // fallback function (if there is one)
+										)); ?>
 									</div>
-									<input type="checkbox" id="hamburg" class="nav-trigger t-h0 d-h0 m-all" role="tooltip">
-																<div id="toggle" role="tooltip">
-																	<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
-																	<div>
+									
+								
 						
 
 					</nav>
