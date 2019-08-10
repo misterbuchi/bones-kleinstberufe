6a6fed13d2f82bbc1a753d561c0af675a23d0db2
Dominik Bucheli <dominik@bucheli.me>
2019-08-10 22:38:11 +0200

    Update page.php

--- page.php
+++ page.php
@@ -15,14 +15,14 @@
 				<li><?php the_title(); ?></li>-->
 				<?php wp_nav_menu(array(
 				    					'container' => '',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-				    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+				    					'container_class' => 'footer-links cf register',         // class of container (should you choose to use it)
 				    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
 				    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
 				    					'theme_location' => 'footer-links',             // where it's located in the theme
 				    					'before' => '',                                 // before the menu
 				    					'after' => '',                                  // after the menu
-				    					'link_before' => '<li>',                            // before each link
-				    					'link_after' => '</li>',                             // after each link
+				    					'link_before' => '',                            // before each link
+				    					'link_after' => '',                             // after each link
 				    					'depth' => 0,                                   // limit the depth of the nav
 				    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
 										)); ?>
