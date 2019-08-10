16e6bf039df2618b9700eac72c5cbb0a11b9ab58
Dominik Bucheli <dominik@bucheli.me>
2019-08-10 22:34:07 +0200

    Update page.php

--- page.php
+++ page.php
@@ -8,11 +8,24 @@
 			
 			<ul class="register">
 				<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Startseite</a></li>
-				<?php if ( $post->post_parent ) { ?>
+				<!--<?php if ( $post->post_parent ) { ?>
 				<li><a href="<?php echo get_permalink( $post->post_parent ); ?>"><?php echo get_the_title( $post->post_parent ); ?></a></li>
 				<?php } ?>
 				
-				<li><?php the_title(); ?></li>
+				<li><?php the_title(); ?></li>-->
+				<?php wp_nav_menu(array(
+				    					'container' => '',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+				    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+				    					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
+				    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+				    					'theme_location' => 'footer-links',             // where it's located in the theme
+				    					'before' => '',                                 // before the menu
+				    					'after' => '',                                  // after the menu
+				    					'link_before' => '<li>',                            // before each link
+				    					'link_after' => '</li>',                             // after each link
+				    					'depth' => 0,                                   // limit the depth of the nav
+				    					'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+										)); ?>
 				
 				
 			</ul>
