5934eeb7c926d88732990197d6c977c6e5b50cbe
Dominik Bucheli <dominik@bucheli.me>
2019-08-10 23:31:19 +0200

    Update page.php

--- page.php
+++ page.php
@@ -18,7 +18,7 @@
 				
 				$menuID = $menuLocations['footer-links']; // Get the *primary* menu ID
 				
-				$primaryNav = wp_get_nav_menu_items($menuID); 
+				$primaryNav = wp_get_nav_menu_items($menuID, array('post_parent' => 0)); 
 				
 				foreach ( $primaryNav as $navItem ) {
 				
