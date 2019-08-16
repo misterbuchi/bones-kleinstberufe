3d1bfa907e4527681be225e40d548b6ccd26f729
Dominik Bucheli <dominik@bucheli.me>
2019-08-16 18:03:21 +0200

    Update single.php

--- single.php
+++ single.php
@@ -51,26 +51,26 @@
 					</main>
 
 
+											<?php get_sidebar(); ?>
 				</div>
-				<?php endwhile; ?>
 				
-										<?php else : ?>
-				
-											<article id="post-not-found" class="hentry cf">
-													<header class="article-header">
-														<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-													</header>
-													<section class="entry-content">
-														<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-													</section>
-													<footer class="article-footer">
-															<p><?php _e( 'This is the error message in the single.php template.', 'bonestheme' ); ?></p>
-													</footer>
-											</article>
-				
-										<?php endif; ?>
-															<?php get_sidebar(); ?>
-										
+						<?php endwhile; ?>
+						
+												<?php else : ?>
+						
+													<article id="post-not-found" class="hentry cf">
+															<header class="article-header">
+																<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+															</header>
+															<section class="entry-content">
+																<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+															</section>
+															<footer class="article-footer">
+																	<p><?php _e( 'This is the error message in the single.php template.', 'bonestheme' ); ?></p>
+															</footer>
+													</article>
+						
+												<?php endif; ?>				
 
 			</div>
 			
