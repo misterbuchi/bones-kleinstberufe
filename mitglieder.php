8f5e78e30ff9dd89533994c55a17f455fd5686cb
Dominik Bucheli <dominik@bucheli.me>
2019-08-16 23:14:55 +0200

    Update mitglieder.php

--- mitglieder.php
+++ mitglieder.php
@@ -22,7 +22,7 @@
 			<div class="Rectangle pale-lilac first"></div>
 			<div class="Rectangle dull-red second"></div>
 			<div class="Rectangle soft-blue third"></div>
-			<div class="Rectangle sick-green thourth"></div>
+			<div class="Rectangle grey-teal thourth"></div>
 			
 			
 			
