1ad4de4018987faa705481165fca1573447b558b
Dominik Bucheli <dominik@bucheli.me>
2019-08-18 10:40:18 +0200

    svg

--- functions.php
+++ functions.php
@@ -304,6 +304,25 @@ function kb_svg ( $svg_mime ){
 
 add_filter( 'upload_mimes', 'kb_svg' );
 
+add_filter( 'wp_get_attachment_image_src', 'fix_wp_get_attachment_image_svg', 10, 4 );  /* the hook */
+
+ function fix_wp_get_attachment_image_svg($image, $attachment_id, $size, $icon) {
+    if (is_array($image) && preg_match('/\.svg$/i', $image[0]) && $image[1] <= 1) {
+        if(is_array($size)) {
+            $image[1] = $size[0];
+            $image[2] = $size[1];
+        } elseif(($xml = simplexml_load_file($image[0])) !== false) {
+            $attr = $xml->attributes();
+            $viewbox = explode(' ', $attr->viewBox);
+            $image[1] = isset($attr->width) && preg_match('/\d+/', $attr->width, $value) ? (int) $value[0] : (count($viewbox) == 4 ? (int) $viewbox[2] : null);
+            $image[2] = isset($attr->height) && preg_match('/\d+/', $attr->height, $value) ? (int) $value[0] : (count($viewbox) == 4 ? (int) $viewbox[3] : null);
+        } else {
+            $image[1] = $image[2] = null;
+        }
+    }
+    return $image;
+} 
+
 add_theme_support( 'custom-logo', array(
 	'height'      => 127,
 	'width'       => 300,
