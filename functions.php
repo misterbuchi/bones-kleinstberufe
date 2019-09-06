71ffb74369769d9bb0a9f790c5926246e1b0062b
Dominik Bucheli <dominik@bucheli.me>
2019-09-06 16:36:12 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -68,15 +68,17 @@ add_filter( 'excerpt_length', function($length) {
 } );
 
 
-function manual_excerpt_more( $excerpt ) {
-	$excerpt_more = '';
-	if( has_excerpt() ) {
-    	$excerpt_more = '&nbsp;<a href="' . get_permalink() . '" rel="nofollow">'. __( '[:de]Mehr erfahren[:fr]Plus[:]', 'bonestheme' ) .'</a>';
+add_filter( 'wp_trim_excerpt', function( $excerpt ) {
+	if ( has_excerpt() ) {
+		$excerpt = sprintf( '%1$s <a href="%2$s">%3$s</a>',
+			$excerpt,
+			get_permalink(),
+                        'Read more'
+		);
 	}
-	return $excerpt . $excerpt_more;
-}
-add_filter( 'get_the_excerpt', 'manual_excerpt_more' );
-
+	
+	return $excerpt;
+} );
 
 /************* OEMBED SIZE OPTIONS *************/
 
