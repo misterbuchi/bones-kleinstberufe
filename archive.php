953989942e1c3e0f5193d3ec4812bb3a14828383
Fussverkehr <dominik.bucheli@fussverkehr.ch>
2020-02-26 16:14:34 +0100

    Update archive.php

--- archive.php
+++ archive.php
@@ -57,8 +57,8 @@
 								    the_post_thumbnail('home-small');
 								}
 								else { ?>
-								    <img class="custom-logo" src="<?php  echo get_stylesheet_directory_uri(); ?>/img/<?php echo __('[:de]Logo_de.svg[:fr]Logo_fr.svg[:]'); ?>"> 
-								<?php }
+								    <img class="custom-logo" src="<?php  echo get_stylesheet_directory_uri(); ?>/img/<?php echo __('[:de]Logo_de.svg[:fr]Logo_fr.svg[:]'); ?>">
+								<?php } ;
 								
 								 ?></a>
 								
